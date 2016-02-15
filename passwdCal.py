196051a7a980a3df0d8fd6b1b21e3c19c741a3c3
modkzs <746061261@qq.com>
2016-02-15 20:23:26 +0800

    add length

--- passwdCal.py
+++ passwdCal.py
@@ -16,7 +16,9 @@ parser = argparse.ArgumentParser(description='Calculate passwd according to user
 
 parser.add_argument('-u', metavar = "string", help = "username")
 parser.add_argument('-a', metavar = "string", help = "application name")
+parser.add_argument('-l', metavar = "int", type=int, help = "password length", default=10)
 
 user = parser.parse_args().u
 app = parser.parse_args().a
-print(getMD5(user + "-" + app).title())
+length = parser.parse_args().l
+print(getMD5(user + "-" + app).title()[:parser.parse_args().l])
